eab6ad089ef2852a13f188400e6a869515f38bc9
DreamCatcher13 <gerey37ng@gmail.com>
2023-10-13 13:12:19 +0300

    refactor: pointers

--- structs/main.go
+++ structs/main.go
@@ -3,15 +3,39 @@ package main
 import "fmt"
 
 // definition
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	//alex := person{firstName: "Alex", lastName: "Anderson"}
-	var alex person // go will assign Zero value to the fields, "", 0, 0, bool==False
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Printf("%+v", alex)
+	//var alex person  >>  go will assign Zero value to the fields, "", 0, 0, bool==False
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "jim@tower.com",
+			zipCode: 94000,
+		},
+	}
+
+	// jimPointer := &jim   & > access to mmr address of jim
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName // * > access to value by mmr adress
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
