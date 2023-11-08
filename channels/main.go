d2c5d8ceff511aa5812a7280fe98ff3eab68cceb
DreamCatcher13 <gerey37ng@gmail.com>
2023-11-08 18:19:09 +0200

    refactor: channels

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,17 +14,28 @@ func main() {
 		"https://go.dev/",
 	}
 
+	c := make(chan string) // creating a channel for communication between go routines
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for l := range c { // wait for a value in channel and assign it to "l"
+		go func(link string) { // func literal => lambda => anonymous func
+			time.Sleep(5 * time.Second)
+			checkLink(link, c) // receive a value from a channel;  blocking code
+		}(l) // call that literal
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
+		c <- link // sending data to channel
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
